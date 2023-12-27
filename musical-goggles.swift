5630a959decdc8f45098f5bf9dc60724310f1742
lisa <150911313+ovcinnikovmatvej335@users.noreply.github.com>
2023-12-28 01:55:33 +0700

    Create musical-goggles.swift

---
+++ musical-goggles.swift
@@ -0,0 +1,4 @@
+   elif chosen_option == "Run away":
+                print("You run away from the monster.")
+                time.sleep(1)
+                self.current_room = self.load_room("start")
